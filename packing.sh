609781720d08a22efc13fa283e7c1a43e7f96ac3
VectorZYJ <yz10317@nyu.edu>
2025-10-21 23:07:03 +0800

    Rename src files

--- packing.sh
+++ packing.sh
@@ -1,5 +1,5 @@
 ## MacOS
-pyinstaller --windowed --onefile --icon="icon.icns" --add-data="icon.icns:." src_macos.py
+pyinstaller --windowed --onefile --icon="icon.icns" --add-data="icon.icns:." src_mac.py
 
 ## Windows
-pyinstaller --windowed --onefile --icon="icon.ico" --add-data="icon.ico;." src_windows.py
+pyinstaller --windowed --onefile --icon="icon.ico" --add-data="icon.ico;." src_win.py
